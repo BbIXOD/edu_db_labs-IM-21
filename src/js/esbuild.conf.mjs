5e4e2eac288ff4f5d156c6ac3b7ff9aa58be48c4
BbIXOD <maksdyachok2005@gmail.com>
2023-12-03 22:19:33 +0200

    js project initialization

---
+++ src/js/esbuild.conf.mjs
@@ -0,0 +1,30 @@
+import { nodeExternalsPlugin } from "esbuild-node-externals";
+import { build } from "esbuild";
+import fs from "fs"
+
+const getFiles = (dir, ext) => {
+  return fs.readdirSync(dir, { recursive: true, encoding: "utf-8",  })
+  .map(file => {
+    return './src/' + file.replace(/\\/g, '/')
+  })
+  .filter(file => file.split('.').pop() === ext)
+}
+
+try {
+  const files = getFiles("./src", "ts")
+
+  build({
+    entryPoints: files,
+    bundle: true,
+    minify: true,
+    platform: "node",
+    sourcemap: true,
+    tsconfig: "tsconfig.json",
+    plugins: [nodeExternalsPlugin()],
+    outdir: "dist",
+  })
+}
+catch (err) {
+  console.log(err)
+  process.exit(1)
+}
