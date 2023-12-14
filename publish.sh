d07a43a1277a4976646b38a2f3762abea08c3ab0
BbIXOD <maksdyachok2005@gmail.com>
2023-12-14 21:12:46 +0200

    prepare for pages deploying

--- publish.sh
+++ publish.sh
@@ -20,12 +20,12 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https:/github.com/BbIXOD/db_coursework-IM-21 master:gh-pages
+git push -f https://github.com/BbIXOD/edu_db_labs-IM-21 master:gh-pages
 
 # if someone uses http request
-# git push -f https://github.com/Dmutre/edu_db_labs-IM-21.git master:gh-pages
+# git push -f https://github.com/BbIXOD/edu_db_labs-IM-21.git master:gh-pages
 
 # if your default branch is main
-# git push -f git@github.com:Dmutre/edu_db_labs-IM-21.git main:gh-pages
+# git push -f git@github.com:BbIXOD/edu_db_labs-IM-21.git main:gh-pages
 
 cd -
